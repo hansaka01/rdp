4107ce6d6e7e6c5bc175529c5379fabd3d49efe1
hansaka01 <113287706+hansaka01@users.noreply.github.com>
2023-05-05 14:02:53 +0530

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -1,4 +1,14 @@
 @echo off
+start .\ngrok\localtonet.exe AuthToken iGve3jXl1qpLsDHyRaABmEb8o9YTdCVN0
+echo --------------------------------Rdp by hansaka--------------------------------
+echo `                                                                             `
+echo `                                                                             `
+echo `                                                                             `
+echo `                                                                             `
+echo `                                                                             `
+echo `-----------------------------------------------------------------------------`
+
+
 :x
 
 goto x
